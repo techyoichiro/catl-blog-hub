fc7c771f32b80aafdfa545652f3a870e189eb3a0
TechYoichiro <y.119.wonder@gmail.com>
2024-10-14 21:34:08 +0900

    Fix responsive

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -11,19 +11,19 @@ import { Member } from "../types";
 
 const MemberCard: React.FC<{ member: Member }> = ({ member }) => {
   return (
-    <Link href={getMemberPath(member.id)} className="member-card relative block text-center text-sm">
-      <div className="member-card__avatar">
+    <Link href={getMemberPath(member.id)} className="relative block text-center text-sm group">
+      <div>
         <Image
           src={member.avatarSrc}
           alt={member.name}
           width={80}
           height={80}
-          className="member-card__avatar-img w-full h-auto"
+          className="rounded-none w-full h-auto block"
         />
       </div>
-      <h2 className="member-card__name mt-3 text-lg">{member.name}</h2>
-      <p className="member-card__bio mt-2 inline-block text-left text-sm leading-6 opacity-70">{member.bio}</p>
-      <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity bg-gray-200"></div>
+      <h2 className="mt-3 text-lg">{member.name}</h2>
+      <p className="mt-2 inline-block text-left text-sm leading-6 opacity-70">{member.bio}</p>
+      <div className="absolute inset-0 opacity-0 group-hover:opacity-30 transition-opacity bg-gray-200"></div>
     </Link>
   );
 };
@@ -33,14 +33,14 @@ const Page: NextPage = () => {
     <>
       <PageSEO title="Members" path="/members" />
       <ContentWrapper>
-        <section className="members py-10">
-          <h1 className="members__title text-3xl">
+        <section className="py-10">
+          <h1 className="text-3xl">
             Members{" "}
-            <span className="members__title-team block text-xl text-gray-500">
+            <span className="block text-xl text-gray-500">
               @ {config.siteMeta.teamName}
             </span>
           </h1>
-          <div className="members__cards mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-10 gap-x-8">
+          <div className="mt-8 grid grid-cols-2 sm:grid-cols-4 gap-y-10 gap-x-8">
             {members.map((member, i) => (
               <MemberCard key={`member-card-${i}`} member={member} />
             ))}
